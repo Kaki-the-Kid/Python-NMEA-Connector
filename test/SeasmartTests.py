bf2341677329c6765cac6cdd0d57ce85fad8e8c0
Kaki-the-Kid <karsten.reitan@gmail.com>
2023-01-24 10:53:59 +0100

    Init commit

--- test/SeasmartTests.py
+++ test/SeasmartTests.py
@@ -28,78 +28,75 @@
 #include <Seasmart.h>
 
 
-TEST_CASE("SEASMART EXPORT", "[seasmart]") {
-  tN2kMsg msg;
+class TEST_CASE("SEASMART EXPORT", "[seasmart]"):
+	tN2kMsg msg:
+	# PGN127257 - "Attitude"
+	# SID: 42 - YAW: 1 degrees - Pitch: 10 degrees - Roll: 30 degrees
+	msg.SetPGN(127257L)
+	msg.Priority=2
+	msg.AddByte(42)
+	msg.Add2ByteDouble(DegToRad(1),0.0001)
+	msg.Add2ByteDouble(DegToRad(10),0.0001)
+	msg.Add2ByteDouble(DegToRad(30),0.0001)
+	msg.AddByte(0xff) # Reserved
 
-  // PGN127257 - "Attitude"
-  // SID: 42 - YAW: 1 degrees - Pitch: 10 degrees - Roll: 30 degrees
-  msg.SetPGN(127257L);
-  msg.Priority=2;
-  msg.AddByte(42);
-  msg.Add2ByteDouble(DegToRad(1),0.0001);
-  msg.Add2ByteDouble(DegToRad(10),0.0001);
-  msg.Add2ByteDouble(DegToRad(30),0.0001);
-  msg.AddByte(0xff); // Reserved
+
+	def SECTION("export to large buffer"):
+		char buffer[512]
+		const char *expectedResult = "$PCDIN,01F119,00000000,0F,2AAF00D1067414FF*59"
+		REQUIRE( N2kToSeasmart(msg, 0, buffer, sizeof(buffer)) == strlen(expectedResult) )
+		REQUIRE( std::string(buffer) == expectedResult )
+	
+
+	SECTION("export to buffer that is exactly the right size"):
+		char buffer[30 + 2*8]
+		const char *expectedResult = "$PCDIN,01F119,00000000,0F,2AAF00D1067414FF*59"
+		REQUIRE( N2kToSeasmart(msg, 0, buffer, sizeof(buffer)) == strlen(expectedResult) )
+		REQUIRE( std::string(buffer) == expectedResult )
+	
+
+	SECTION("export to buffer that is too small"):
+		buffer[10] #char
+
+    REQUIRE( N2kToSeasmart(msg, 0, buffer, sizeof(buffer)) == 0 )
 
 
-  SECTION("export to large buffer") {
-    char buffer[512];
-    const char *expectedResult = "$PCDIN,01F119,00000000,0F,2AAF00D1067414FF*59";
-    REQUIRE( N2kToSeasmart(msg, 0, buffer, sizeof(buffer)) == strlen(expectedResult) );
-    REQUIRE( std::string(buffer) == expectedResult );
-  }
+class TEST_CASE("SEASMART IMPORT"):
+	tN2kMsg msg
+	timestamp = 1337 #uint32_t 
 
-  SECTION("export to buffer that is exactly the right size") {
-    char buffer[30 + 2*8];
-    const char *expectedResult = "$PCDIN,01F119,00000000,0F,2AAF00D1067414FF*59";
-    REQUIRE( N2kToSeasmart(msg, 0, buffer, sizeof(buffer)) == strlen(expectedResult) );
-    REQUIRE( std::string(buffer) == expectedResult );
-  }
+	def SECTION("read valid message"):
+		message = "$PCDIN,01F119,00000000,0F,2AAF00D1067414FF*59" #const char *
 
-  SECTION("export to buffer that is too small") {
-    char buffer[10];
+		REQUIRE( SeasmartToN2k(message, timestamp, msg) )
+		REQUIRE( msg.PGN == 127257L )
+			int index = 0
+		REQUIRE( msg.GetByte(index) == 42 )
+		REQUIRE( msg.Get2ByteDouble(0.0001, index) == Approx(DegToRad(1)).margin(0.0001) )
+		REQUIRE( msg.Get2ByteDouble(0.0001, index) == Approx(DegToRad(10)).margin(0.0001) )
+		REQUIRE( msg.Get2ByteDouble(0.0001, index) == Approx(DegToRad(30)).margin(0.0001) )
+		REQUIRE( msg.GetByte(index) == 0xFF )
+		REQUIRE( timestamp == 0)
 
-    REQUIRE( N2kToSeasmart(msg, 0, buffer, sizeof(buffer)) == 0 );
-  }
-}
+		# Note: Seasmart format does not include priority!
+		# REQUIRE( msg.Priority == 2 )
 
-TEST_CASE("SEASMART IMPORT") {
-  tN2kMsg msg;
-  uint32_t timestamp = 1337;
 
-  SECTION("read valid message") {
-    const char *message = "$PCDIN,01F119,00000000,0F,2AAF00D1067414FF*59";
+	def first_SECTION("read valid message with lower case hexadecimal"):
+    	message = "$PCDIN,01f119,00000000,0f,2aaf00d1067414ff*59" #const char *
 
-    REQUIRE( SeasmartToN2k(message, timestamp, msg) );
-    REQUIRE( msg.PGN == 127257L );
-    int index = 0;
-    REQUIRE( msg.GetByte(index) == 42 );
-    REQUIRE( msg.Get2ByteDouble(0.0001, index) == Approx(DegToRad(1)).margin(0.0001) );
-    REQUIRE( msg.Get2ByteDouble(0.0001, index) == Approx(DegToRad(10)).margin(0.0001) );
-    REQUIRE( msg.Get2ByteDouble(0.0001, index) == Approx(DegToRad(30)).margin(0.0001) );
-    REQUIRE( msg.GetByte(index) == 0xFF );
-    REQUIRE( timestamp == 0);
+    	REQUIRE( SeasmartToN2k(message, timestamp, msg) )
+    	REQUIRE( msg.PGN == 127257L )
 
-    // Note: Seasmart format does not include priority!
-    //REQUIRE( msg.Priority == 2 );
-  }
 
-  SECTION("read valid message with lower case hexadecimal") {
-    const char *message = "$PCDIN,01f119,00000000,0f,2aaf00d1067414ff*59";
+	def second_SECTION("read message with invalid checksum"):
+		message = "$PCDIN,01F119,00000000,0F,2AAF00D1067414FF*99" #const char *
 
-    REQUIRE( SeasmartToN2k(message, timestamp, msg) );
-    REQUIRE( msg.PGN == 127257L );
-  }
+    	REQUIRE( !SeasmartToN2k(message, timestamp, msg) )
 
-  SECTION("read message with invalid checksum") {
-    const char *message = "$PCDIN,01F119,00000000,0F,2AAF00D1067414FF*99";
 
-    REQUIRE( !SeasmartToN2k(message, timestamp, msg) );
-  }
+	def third_SECTION("read message with empty string"):
+		message = "" #const char *
 
-  SECTION("read message with empty string") {
-    const char *message = "";
+    	REQUIRE( !SeasmartToN2k(message, timestamp, msg) );
 
-    REQUIRE( !SeasmartToN2k(message, timestamp, msg) );
-  }
-}
