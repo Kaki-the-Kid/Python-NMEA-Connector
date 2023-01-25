43e814f7a96b8c9524a62c5d3ad277e14ff60609
Kaki-the-Kid <karsten.reitan@gmail.com>
2023-01-25 17:41:52 +0100

    Bit cleaning up

--- NMEA2000.py
+++ NMEA2000.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from multiprocessing import Process
+import os
+
 import NMEA2000_defs
 
 
--- python-sudo.sh
+++ python-sudo.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# This script is used to run python scripts as root
+# Remember to make it executable
+# chmod +x python-sudo.sh
 sudo /usr/bin/python "$@"
