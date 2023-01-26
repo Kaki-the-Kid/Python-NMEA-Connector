b5cf9c539b1c4a0f9a662a81687923f8bb5d095e
Kaki-the-Kid <karsten.reitan@gmail.com>
2023-01-26 09:43:50 +0100

    Init commit

--- NMEA2000.py
+++ NMEA2000.py
@@ -3,6 +3,13 @@
 from multiprocessing import Process
 import os
 
+# Import NMEA2000 modules
 import NMEA2000_defs
 
+from modulefinder import Module
 
+from Modules.Update import Update
+from Modules.UpdateLog import UpdateLog
+from Modules.UpdateUnicorn import UniCorn, UpdateIcon
+from Modules.init_unicorn import unicorn_init
+from Modules.update_matrix import *
