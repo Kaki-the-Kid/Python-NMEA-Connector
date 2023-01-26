94d981d2c8d7b476ee3eadfaace1df9f894e6c6c
Kaki-the-Kid <karsten.reitan@gmail.com>
2023-01-26 10:39:09 +0100

    Threading for main

--- NMEA2000.py
+++ NMEA2000.py
@@ -2,14 +2,62 @@
 # -*- coding: utf-8 -*-
 from multiprocessing import Process
 import os
+import threading
 
-# Import NMEA2000 modules
 import NMEA2000_defs
+#import NMEA2000_Receive
+#import NMEA2000_Send
 
-from modulefinder import Module
+import modules.Config.Config as Config
+       
+# Import NMEA2000 modules
+from modules.update.Update import Update
+from modules.update.UpdateLog import UpdateLog
+from modules.update.UpdateUnicorn import UniCorn, UpdateIcon
+from modules.update.init_unicorn import unicorn_init
+from modules.update.update_matrix import *
 
-from Modules.Update import Update
-from Modules.UpdateLog import UpdateLog
-from Modules.UpdateUnicorn import UniCorn, UpdateIcon
-from Modules.init_unicorn import unicorn_init
-from Modules.update_matrix import *
+# Global variables
+processes_bar = []
+
+def quit_all_threads():
+    for thread in threads:
+        thread.cancel()
+        thread.join()
+        
+
+def quit_all_processes():
+    for process in processes:
+        process.terminate()
+        process.join()
+        
+def main():
+
+    main_thread = threading.Timer(THREADING_TIMER, main)
+
+    main_thread.start()
+
+    threads.append(main_thread)
+
+    try:
+
+        Update().update_json()
+
+        UpdateLog().create_log()
+
+        update_matrix()
+
+        p = Process(target=update_bargraph)
+
+        for process in processes_bar:
+            process.terminate()
+
+        p.start()
+        processes_bar.append(p)
+
+        UpdateIcon().set_icon_path()
+
+    except KeyboardInterrupt:
+        
+        quit_all()
+        clear_all()
