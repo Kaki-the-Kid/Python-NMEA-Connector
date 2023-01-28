43d6cf6e3d7bb0869e79c019cc0a1796dc0ff059
Kaki-the-Kid <karsten.reitan@gmail.com>
2023-01-28 17:18:41 +0100

    Starting port from C# to Python

---
+++ Modules/NMEA_Devices/views/NMEA0183_Device_Properties.py
@@ -0,0 +1,118 @@
+'''
+<summary>
+Interaction logic for NMEA2000_Device_Properties.xaml
+</summary>
+'''
+#from tkinter import *
+from tkinter import Window
+
+class NMEA2000_Device_Properties(Window):
+    def NMEA2000_Device_Properties():
+        InitializeComponent()
+
+
+    def TextBox_TextChanged(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def Button_Click(object sender, RoutedEventArgs e):
+        return None
+
+
+    def TextBox_ProductCode(object sender, TextChangedEventArgs e):
+        return None
+
+    
+    def TextBox_DatabaseVersion(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_ModelVersion(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_ModelID(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_SoftwareVersion(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_EquipmentSerial(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_EquipmentCertification(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_EquipmentLEN(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_CAN1(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_CAN2(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_CAN3(object sender, TextChangedEventArgs e):
+        return None
+        
+        
+    def Button_RefreshAll(object sender, RoutedEventArgs e):
+        return None
+
+
+    def Button_UpdateConfigInfo(object sender, RoutedEventArgs e):
+        return None
+
+
+    def TextBox_TextChanged_1(object sender, TextChangedEventArgs e):
+        return None
+
+
+    def TextBox_CAN1Indicator(object sender, TextChangedEventArgs e):
+        throw new NotImplementedException()
+
+
+    def TextBox_CAN1Update(object sender, TextChangedEventArgs e):
+        throw new NotImplementedException()
+        
+        
+    def Button_AddressClaimUpdate(object sender, RoutedEventArgs e):
+
+
+    def TextBox_InstallationDescription1(object sender, TextChangedEventArgs e):
+        throw new NotImplementedException()
+
+    def TextBox_InstallationDescription2(object sender, TextChangedEventArgs e):
+        throw new NotImplementedException()
+
+
+    def TextBox_InstallationDescription3(object sender, TextChangedEventArgs e):
+        return None
+    
+    def TextBox_ManufacturerInfo(object sender, TextChangedEventArgs e):
+        throw new NotImplementedException()
+        
+    def TextBox_ManufacturerModelCode(object sender, TextChangedEventArgs e):    
+
+        private void TextBox_EquipmentIdicator(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    def TextBox_EquipmentUpdate(object sender, TextChangedEventArgs e):
+        throw new NotImplementedException();
+
+
+    def TextBox_CAN2Idicator(object sender, TextChangedEventArgs e):
+        throw new NotImplementedException();
+
+
+    def Button_Click_1(object sender, RoutedEventArgs e):
+        throw new NotImplementedException()
---
+++ Modules/NMEA_Devices/views/NMEA2000_Device_Properties.py
@@ -0,0 +1,127 @@
+'''
+NMEA2000_Device_Properties.py
+Interaction logic for NMEA2000_Device_Properties.xaml
+'''
+
+class NMEA2000_Device_Properties(Window):
+    
+    def __init__(self) -> None:
+        super().__init__()
+        object.__init__(self)
+        self.InitializeComponent()
+        
+        sender@app.route("route")
+        self.sender = sender
+        self.e = e
+
+
+    def TextBox_TextChanged(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def Button_Click(object sender, RoutedEventArgs e):
+        pass
+
+
+    def TextBox_ProductCode(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def TextBox_DatabaseVersion(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def TextBox_ModelVersion(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def TextBox_ModelID(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def TextBox_SoftwareVersion(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def TextBox_EquipmentSerial(object sender, TextChangedEventArgs e):
+        pass    
+
+
+    def TextBox_EquipmentCertification(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def TextBox_EquipmentLEN(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def TextBox_CAN1(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def TextBox_CAN1Indicator(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def TextBox_CAN2(object sender, TextChangedEventArgs e):
+        pass
+
+
+    def Button_RefreshAll(object sender, RoutedEventArgs e):
+        pass
+
+
+    def Button_UpdateConfigInfo(object sender, RoutedEventArgs e):
+        pass
+
+
+    def TextBox_TextChanged_1(object sender, TextChangedEventArgs e):
+        pass
+
+
+
+    def TextBox_CAN1Indicator(object sender, TextChangedEventArgs e):
+        #throw new NotImplementedException()
+        pass
+
+
+    def TextBox_CAN2Indicator(object sender, TextChangedEventArgs e):
+        #throw new NotImplementedException()
+        pass
+        
+        
+    def Button_AddressClaimUpdate(object sender, RoutedEventArgs e):
+        pass
+
+
+    def TextBox_InstallationDescription1(object sender, TextChangedEventArgs e):
+        #throw new NotImplementedException()
+        pass
+
+
+    def TextBox_InstallationDescription2(object sender, TextChangedEventArgs e):
+        #throw new NotImplementedException()
+        pass
+
+
+    def TextBox_ManufacturerInfo(object sender, TextChangedEventArgs e):
+        #throw new NotImplementedException()
+        pass
+
+
+    def TextBox_EquipmentIdicator(object sender, TextChangedEventArgs e):
+        #throw new NotImplementedException()
+        pass
+
+
+    def TextBox_EquipmentUpdate(object sender, TextChangedEventArgs e):
+        #throw new NotImplementedException()
+        pass
+
+
+    def TextBox_CAN2Idicator(object sender, TextChangedEventArgs e):
+        #throw new NotImplementedException()
+        pass
+
+    def Button_Click_1(object sender, RoutedEventArgs e):
+        pass
