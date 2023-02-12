ffef61c2525f2bd7eb27e2f31ebc8a8f211a9155
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-02-12 16:09:27 +0100

    Init commit

--- Modules/NMEA_Interpreter/NMEA_0183_Commands/ROT.py
+++ Modules/NMEA_Interpreter/NMEA_0183_Commands/ROT.py
@@ -30,72 +30,36 @@ from NMEA0183_Commands import NMEA0183_Command
 
 class ROT(NMEAMessage):
     
-    def __init__(self):
-        self.__rot = 0.0
+    def __init__(self, sentence):
+        self._datasentence = sentence.split(',')
+        if (self._datasentence.lenght != 3): # Check if the sentence is valid
+            raise Exception("Invalid ROT sentence: {}".sentence)
+            self._valid = False
+        # Message ID: 
+        self._messageid = self._datasentence[0]
+        # Rate of Turn: 
+        self._rot = self._datasentence[1]
+        # Status: 
+        self._status = self._datasentence[2]
     
+    
+    def set_messageid(self, messageid):
+        self._messageid = messageid
+        
+    def get_messageid(self):
+        return self._messageid
+        
     def set_rot(self, rot):
-        self.__rot = rot
+        self._rot = rot
     
     def get_rot(self):
-        return self.__rot
+        return self._rot
     
-
-
-    
-    list: _waypoints = []
-
-    # Waypoint tpe   
-    def WaypointListType(Enum):
-        CompleteWaypointsList,# Complete list of waypoints
-        RemainingWaypointsList# List of remaining waypoints
-
-    
-    # Initializes a new instance of the <see cref="Rte"/> class.
-    
-    # <param name="type">The message type</param>
-    # <param name="message">The NMEA message values.</param>
-    def RTE(str: type, message = []):
-        if (message == null or message.Length < 4):
-            raise Exception("Invalid RTE", "message")
+    def set_status(self, status):
+        self._status = status
         
-        ListType = WaypointListType.CompleteWaypointsList if (message[2] == "c") else WaypointListType.RemainingWaypointsList
-        RouteId = message[3]
-        pass
-
-    # <inheritdoc />
-    def ParseSentences( talker, message = [] ):
-        if ( self.MessageParts[2] != message[2] or self.MessageParts[3] != message[3] ):
-            return False
-            
-        _waypoints.AddRange(message.Skip(4))
-        return True
-
-    
-    # Gets the type of the list.
-    def ListType(): # WaypointListType
-        return self.ListType
-
-    
-    # Gets the route identifier.
-    def RouteId(): 
-        return self.RouteId
-
-    
-    # Waypoints
-    #def IReadOnlyList<str> Waypoints => _waypoints.AsReadOnly()
-
-    
-    # @brief    Returns an enumerator that iterates through the collection.
-    # @returns A System.Collections.Generic.IEnumerator{T} that can be used to iterate through the collection.</returns>
-    #def IEnumerator<str> IEnumerable<str>.GetEnumerator()
-    #    foreach (str waypoint in Waypoints)
-    #        yield return waypoint
-
-    
-    # Returns an enumerator that iterates through a collection.
-    # @returns An System.Collections.IEnumerator object that can be used to iterate through the collection.</returns>
-    #def System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-    #    return self.GetEnumerator()
+    def get_status(self):
+        return self._status
 
     def test_code():
         # create a new rate of turn object
